11c480024575fb17c578c88adcb25ee8569d87d4
nikolholicka <nikol.holicka@gmail.com>
2019-05-15 15:03:24 +0100

    lab completion

--- mod1/section07/dsc-1-07-07-object-oriented-shopping-cart-lab-online-ds-pt-041519/shopping_cart.py
+++ mod1/section07/dsc-1-07-07-object-oriented-shopping-cart-lab-online-ds-pt-041519/shopping_cart.py
@@ -1,17 +1,34 @@
 class ShoppingCart:
-    # write your code here
+    
     def __init__(self, emp_discount=None):
-      pass
+        self.total = 0
+        self.emp_discount = emp_discount
+        self.items = []
+       
     def add_item(self, name, price, quantity=1):
-       pass
+        for item in list(range(quantity)):
+            self.items.append({'name_of_item':name, 'price_of_item':price})
+            self.total = self.total+price   
+        print(self.total)
+        print(self.items)
+ 
     def mean_item_price(self):
-       pass
+        return self.total/len(self.items)
 
     def median_item_price(self):
-        pass
-
+        list_for_median = [item["price_of_item"] for item in self.items]
+        import statistics
+        return statistics.median(list_for_median)
+     
     def apply_discount(self):
-       pass
+        if self.emp_discount == None:
+            return("Sorry, there is no discount!")
+        else:
+            return (self.total-(self.total*self.emp_discount/100))
 
     def void_last_item(self):
-        pass
+        if len(self.items) == 0:
+            return 'Your cart is empty!'
+        else:
+            self.items.pop()
+            return self.items
